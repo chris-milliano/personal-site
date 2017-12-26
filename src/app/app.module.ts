45d148878872ba758192be24e4ac6338ecd17d65
Chris Milliano <MillianoC@gmail.com>
2017-12-26 10:31:41 -0600

    FEAT: Add page routing fxnality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { ContactPageComponent } from './pages/contact-page/contact-page.component';
+
+const routes: Routes = [
+    { path: '', component: LandingPageComponent },
+    { path: 'contact', component: ContactPageComponent }
+]
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingPageComponent,
+    ContactPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
